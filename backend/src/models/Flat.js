6ca491e1ec6a1b25b3b3df190ee4c14fae4e809e
durvesh159 <durveshraneja13@gmail.com>
2025-11-19 22:59:42 +0530

    Files added

--- backend/src/models/Flat.js
+++ backend/src/models/Flat.js
@@ -1,17 +1,23 @@
 // backend/src/models/Flat.js
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const flatSchema = new mongoose.Schema({
   ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "Resident", required: true },
   ownerName: { type: String, required: true },
 
   bhk: { type: Number, required: true },
-  area: { type: Number, required: true },  
+  area: { type: Number, required: true },
   price: { type: Number, required: true },
-  furnished: { type: String, enum: ["Unfurnished", "Semi", "Fully"], required: true },
+
+  furnished: { 
+    type: String, 
+    enum: ["Unfurnished", "Semi", "Fully"], 
+    required: true 
+  },
+
   availableFrom: { type: Date, required: true },
 
-  images: [{ type: String }],   // BASE64 IMAGES STORED HERE
+  images: [{ type: String }],   // BASE64 IMAGES
 
   featured: { type: Boolean, default: false },
   isRented: { type: Boolean, default: false },
@@ -20,4 +26,4 @@ const flatSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-export default mongoose.model("Flat", flatSchema);
+module.exports = mongoose.model("Flat", flatSchema);
