edbb05ebd1388a288ee9284513ac82517faa80cf
durvesh159 <durveshraneja13@gmail.com>
2025-11-25 11:51:36 +0530

    Files added

--- backend/src/server.js
+++ backend/src/server.js
@@ -191,6 +191,12 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+// ADD THIS
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store");
+  next();
+});
+
 
 app.set('io', io);
 
@@ -222,12 +228,6 @@ io.on('connection', (socket) => {
     console.log('User disconnected:', socket.id);
   });
 });
-
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-store");
-  next();
-});
-
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log('Server running on', PORT));
